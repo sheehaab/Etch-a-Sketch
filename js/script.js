5e0e5c7aa195428d1e7b0db53d0b19b168f9a214
Shehab <shehabahmed963@gmail.com>
2020-10-21 11:52:47 +0200

    make the reset button without the asking task

--- js/script.js
+++ js/script.js
@@ -1,16 +1,34 @@
 const gridHtml = document.querySelector('#etch-grid');
 
-
 for(let i=0;i<16;i++){
 	
 	for(let j=0;j<16;j++){
-		const grid = document.createElement('span');
-		grid.style.cssText = 'width:10px;height:10px;border:1px solid #000;display:inline-block;';
-		grid.classList.add('removeLineHeight')
+		const grid = document.createElement('div');
+		/*Remeber to make the style in the css style not inline*/
+		grid.style.cssText = 'width:30px;height:30px;border:1px solid #000;display:inline-block;';
 		gridHtml.appendChild(grid);
+		grid.classList.add('div-grid');
 	}
-	const BreakEl = document.createElement('br');
-	gridHtml.appendChild(BreakEl)
 	
 }
+
+
+const gridEl = document.querySelectorAll('.div-grid');
+
+gridEl.forEach((div)=>{
+	div.addEventListener('mouseover',function(){
+		div.classList.add('black');
+	})
+})
+
+const btn = document.querySelector('#btn');
+
+btn.addEventListener('click',function(){
+	gridEl.forEach((div)=>{
+		div.classList.remove('black');
+	})
+})
+
+
+
 
