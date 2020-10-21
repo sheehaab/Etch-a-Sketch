1cf9151522179aa2fd7caf29b0f860e4badaec4c
Shehab <shehabahmed963@gmail.com>
2020-10-21 11:15:04 +0200

    make the html line-height is 0 becuase normalize is making it 1.5 and cannot amke changes in other elemens like the span or div because html tag is heigher above them so i made this change to make the grid

---
+++ js/script.js
@@ -0,0 +1,16 @@
+const gridHtml = document.querySelector('#etch-grid');
+
+
+for(let i=0;i<16;i++){
+	
+	for(let j=0;j<16;j++){
+		const grid = document.createElement('span');
+		grid.style.cssText = 'width:10px;height:10px;border:1px solid #000;display:inline-block;line-height:0;';
+		grid.classList.add('removeLineHeight')
+		gridHtml.appendChild(grid);
+	}
+	const BreakEl = document.createElement('br');
+	gridHtml.appendChild(BreakEl)
+	
+}
+
