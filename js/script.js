1788a4828e6cabde536e65cf96f8c6455e2bee4c
Shehab <shehabahmed963@gmail.com>
2020-10-21 11:18:53 +0200

    knowing where were the problem it was because i was changing the span or div line-height but I must make this modifiying to the container div of this grid

--- js/script.js
+++ js/script.js
@@ -5,7 +5,7 @@ for(let i=0;i<16;i++){
 	
 	for(let j=0;j<16;j++){
 		const grid = document.createElement('span');
-		grid.style.cssText = 'width:10px;height:10px;border:1px solid #000;display:inline-block;line-height:0;';
+		grid.style.cssText = 'width:10px;height:10px;border:1px solid #000;display:inline-block;';
 		grid.classList.add('removeLineHeight')
 		gridHtml.appendChild(grid);
 	}
